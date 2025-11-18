914e88ac78e41674e83e7ed1b1b65a594be7af56
Mohammad Anas <mdanascal@gmail.com>
2025-11-18 18:32:49 +0530

    complete auto-focus mini project

--- auto-focus/src/App.jsx
+++ auto-focus/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef, useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const focusRef = useRef();
+  const [value, setvalue] = useState("");
+  useEffect(() => {
+    focusRef.current.focus();
+  }, []);
+  const handleInput = (e) => {
+    setvalue(e.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <input type="text" value={value} disabled />
+      <input
+        type="text"
+        onChange={handleInput}
+        ref={focusRef}
+        className="border border-black"
+        value={value}
+      />
+      <button
+        onClick={() => {
+          focusRef.current.focus();
+        }}
+      >
+        Focus
+      </button>
+      <button
+        onClick={() => {
+          setvalue("");
+        }}
+        value={value}
+      >
+        clear
+      </button>
+    </div>
+  );
+};
 
-export default App
+export default App;
