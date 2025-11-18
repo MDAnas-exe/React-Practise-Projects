1c2c983d9af7af828e93908dc84585408ab0804d
Mohammad Anas <mdanascal@gmail.com>
2025-11-18 23:15:06 +0530

    complete character counter mini project

--- character-counter/src/App.jsx
+++ character-counter/src/App.jsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [value, setvalue] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <div
+        className={`fixed left-1/2 -translate-x-1/2 transition-all duration-300 ${
+          value.length < 15 ? "-top-5" : "top-0"
+        }`}
+      >
+        Max limit is 15 characters
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <input
+        className={`transition duration-300 border border-black ${
+          value.length > 15 ? "text-red-600" : ""
+        }`}
+        type="text"
+        value={value}
+        onChange={(e) => {
+          if (e.target.value.length <= 15) setvalue(e.target.value);
+        }}
+      />
+      <fieldset
+        className={`transition duration-300 border p-1 inline border-black ${
+          value.length > 15 ? "text-red-600" : ""
+        }`}
+      >
+        <legend>Character Count</legend>
+        {value.length}
+      </fieldset>
+      <fieldset
+        className={`transition duration-300 border p-1 inline border-black ${
+          value.length > 15 ? "text-red-600" : ""
+        }`}
+      >
+        <legend>Word Count</legend>
+        {value.trim() === "" ? 0 : value.trim().split(/\s+/).length}
+      </fieldset>
+      <div
+        className="bg-red-600 transition-all duration-300"
+        style={{ width: `${(value.length / 15) * 100}%` }}
+      >
+        &nbsp;
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
