1c581c7438aed44dff591d42450208e30fdd2c19
Mohammad Anas <mdanascal@gmail.com>
2025-11-28 20:16:39 +0530

    complete expensive-calc mini project

--- expensive-calc/src/App.jsx
+++ expensive-calc/src/App.jsx
@@ -1,23 +1,39 @@
-import React, { useMemo, useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
+
+const heavyCalculation = (n) => {
+  n = parseInt(n, 10);
+  let result = 0;
+  for (let i = 0; i < n * 1000000; i++) {
+    result += i % 10;
+  }
+  return result;
+};
 
 const App = () => {
   const [value, setValue] = useState({ number: 0, text: "" });
+
+  const result = useMemo(() => heavyCalculation(value.number), [value.number]);
+
   return (
     <div>
-      <input
-        type="number"
-        placeholder="Enter a number"
-        value={value.number}
-        onChange={(e) => setValue({ ...value, number: e.target.value })}
-      />
-      <span>Result: {value.number}</span>
-      <input
-        type="text"
-        placeholder="Enter a text"
-        value={value.text}
-        onChange={(e) => setValue({ ...value, text: e.target.value })}
-      />
-      <span>Entered text: {value.text}</span>
+      <div>
+        <input
+          type="number"
+          placeholder="Enter a number"
+          value={value.number}
+          onChange={(e) => setValue({ ...value, number: e.target.value })}
+        />
+        <span>Result: {result}</span>
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a text"
+          value={value.text}
+          onChange={(e) => setValue({ ...value, text: e.target.value })}
+        />
+        <span>Entered text: {value.text}</span>
+      </div>
     </div>
   );
 };
